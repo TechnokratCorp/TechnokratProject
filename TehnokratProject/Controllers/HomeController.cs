49a568cd8c7276611f0363070261c4483d96aafa
Dmitry <kurson.d@gmail.com>
2025-01-27 17:11:49 +0200

    Add project files.

---
+++ TehnokratProject/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TehnokratProject.Models;
+
+namespace TehnokratProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+
+
+
+
+
+
+
+        public IActionResult Phone()
+        {
+            return View();
+        }
+
+        public IActionResult Laptop()
+        {
+            return View();
+        }
+
+        public IActionResult TV()
+        {
+            return View();
+        }
+
+        public IActionResult Tablet()
+        {
+            return View();
+        }
+
+        public IActionResult PC()
+        {
+            return View();
+        }
+        public IActionResult Printer()
+        {
+            return View();
+        }
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
