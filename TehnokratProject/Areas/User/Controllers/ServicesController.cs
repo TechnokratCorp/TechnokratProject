cf927bef01b86505aa59abee6789523cfa35cf78
Dmitry <kurson.d@gmail.com>
2025-05-11 16:48:12 +0300

    Finished developing routing in services

--- TehnokratProject/Areas/User/Controllers/ServicesController.cs
+++ TehnokratProject/Areas/User/Controllers/ServicesController.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TehnokratProject.Data;
+using TehnokratProject.Models;
 
 namespace TehnokratProject.Areas.User.Controllers
 {
     [Area("User")]
     public class ServicesController:Controller
     {
-      
+
         public IActionResult List()
         {
             return View();
@@ -27,6 +30,11 @@ namespace TehnokratProject.Areas.User.Controllers
         }
 
         public IActionResult TV()
+        {
+            return View();
+        }
+
+        public IActionResult PC()
         {
             return View();
         }
